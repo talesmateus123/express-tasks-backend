97247d995746096b17f8a585a6be41a5b39597b4
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-02 00:23:26 -0400

    Implemented user api functions, middleware config and routes

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
+consign()
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
 
 app.listen(3000)
