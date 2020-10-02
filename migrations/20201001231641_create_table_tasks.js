56bd0703f8cc238e5aa4c3aae5d573b66ea9111f
Tales Ferreira <talesmateus1999@hotmail.com>
2020-10-01 23:55:26 -0400

    Configurated knexfile and create twonew migrations

---
+++ migrations/20201001231641_create_table_tasks.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('tasks', table => {
+        table.increments('id').primary()
+        table.string('desc').notNull()
+        table.datetime('estimateAt').notNull()
+        table.datetime('doneAt').notNull()
+        table.integer('userId').references('id').
+            inTable('users').notNull()
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('tasks')
+};
